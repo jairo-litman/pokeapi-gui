0519c9daa230410a2b5a63b1656bca9992a7d0c4
Jairo Litman <130161309+jairo-litman@users.noreply.github.com>
2024-05-27 13:05:16 -0300

    skeleton

---
+++ src/components/page-footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex bottom-0 left-0 right-0 w-full h-footer p-4 mt-8 bg-blue-500 items-center justify-center">
+            <h1>Footer</h1>
+        </footer>
+    );
+}
---
+++ src/components/page-header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex top-0 left-0 right-0 shadow-md p-4 h-header w-full bg-red-600 items-center justify-center">
+            <h1>Header</h1>
+        </header>
+    );
+}
