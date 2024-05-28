ad1dc4cc4a01c7af989df8d7c268538b0fcde69e
Jairo Litman <130161309+jairo-litman@users.noreply.github.com>
2024-05-28 15:09:07 -0300

    header

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,7 +1,13 @@
+import Image from 'next/image';
+import PokeApiLogo from '../../public/pokeapi_192_square.png';
+import Link from 'next/link';
+
 export default function Header() {
     return (
-        <header className="flex top-0 left-0 right-0 shadow-md p-4 h-header w-full bg-red-600 items-center justify-center">
-            <h1>Header</h1>
+        <header className="flex top-0 left-0 right-0 shadow-md p-4 h-header w-full bg-secondary items-center justify-center">
+            <Link href="/">
+                <Image src={PokeApiLogo} alt="PokeAPI Logo" width={192} height={192} />
+            </Link>
         </header>
     );
 }
