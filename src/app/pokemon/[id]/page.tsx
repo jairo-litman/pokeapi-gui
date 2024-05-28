2773fa3bf738e4383b5c1df6df5838c5111cca46
Jairo Litman <130161309+jairo-litman@users.noreply.github.com>
2024-05-28 16:18:32 -0300

    native fetch

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,12 +1,14 @@
-import { PokemonClient } from "pokenode-ts";
+import { Pokemon, PokemonSpecies } from "pokenode-ts";
 import PokeInfo from "@/components/poke-info";
 
 export default async function PokemonInfo({ params }: { params: { id: string } }) {
     const id = parseInt(params.id);
 
-    const client = new PokemonClient();
-    const pokemon = await client.getPokemonById(id);
-    const species = await client.getPokemonSpeciesById(id);
+    const speciesUrl = new URL("https://pokeapi.co/api/v2/pokemon-species/" + id);
+    const pokemonUrl = new URL("https://pokeapi.co/api/v2/pokemon/" + id);
+    const species: PokemonSpecies = await fetch(speciesUrl).then((res) => res.json());
+    const pokemon: Pokemon = await fetch(pokemonUrl).then((res) => res.json());
+
 
     return <PokeInfo pokemon={pokemon} species={species} />;
 }
