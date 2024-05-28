3c045b59da79bbcd5e60c7b8d01130e22bbf84ff
Jairo Litman <130161309+jairo-litman@users.noreply.github.com>
2024-05-28 00:14:04 -0300

    native fetch instead of library

--- src/app/list/[slug]/page.tsx
+++ src/app/list/[slug]/page.tsx
@@ -1,20 +1,35 @@
-import { PokemonClient } from "pokenode-ts";
+import { NamedAPIResourceList, Pokemon, PokemonSpecies } from "pokenode-ts";
 
 import PokeCard from "@/components/poke-card";
 import PokePagination from "@/components/poke-pagination";
 
-export default async function List({ params }: { params: { slug: string } }) {
-    const PAGE_SIZE = 30;
+const PAGE_SIZE = 30;
 
+export default async function List({ params }: { params: { slug: string } }) {
     const pageNumber = parseInt(params.slug);
 
-    const client = new PokemonClient();
-    const list = await client.listPokemonSpecies(PAGE_SIZE * (pageNumber - 1), PAGE_SIZE);
+    // We're using Next.js's server-side components, so we fetch directly without React hooks or effects 
+
+    // We could use the PokemonClient class from pokenode-ts, but I want to take advantage of Next.js's
+    // built-in fetch caching and error handling. So pokenode-ts is only used for types and interfaces.
+
+    // Construct the URL for the API request
+    const url = new URL("https://pokeapi.co/api/v2/pokemon-species");
+    url.searchParams.set("offset", String((pageNumber - 1) * PAGE_SIZE));
+    url.searchParams.set("limit", String(PAGE_SIZE));
+
+    // Fetch the list of Pokemon species
+    const list: NamedAPIResourceList = await fetch(url).then((res) => res.json());
+    // Calculate the total number of pages
     const totalPages = Math.ceil(list.count / PAGE_SIZE);
 
-    const pokemonsData = await Promise.all(list.results.map(async (pokemonRaw) => {
-        const species = await client.getPokemonSpeciesByName(pokemonRaw.name);
-        const pokemon = await client.getPokemonById(species.id);
+    // Fetch the data for each Pokemon species, there's not a single endpoint that returns all the data we need
+    const pokemonsData = await Promise.all(list.results.map(async (rawSpecies) => {
+        const species: PokemonSpecies = await fetch(rawSpecies.url).then((res) => res.json());
+
+        const url = new URL("https://pokeapi.co/api/v2/pokemon/" + species.id);
+        const pokemon: Pokemon = await fetch(url).then((res) => res.json());
+
         return { species, pokemon };
     }));
 
@@ -22,7 +37,7 @@ export default async function List({ params }: { params: { slug: string } }) {
         <div className="p-8 min-h-screen">
             <PokePagination currentPage={pageNumber} totalPages={totalPages} />
             <div className="grid grid-cols-6 gap-x-8 gap-y-4 py-4 items-center justify-items-center">
-                {pokemonsData.map((data) => <PokeCard key={data.pokemon.id} pokemon={data.pokemon} species={data.species} />)}
+                {pokemonsData.map(({ species, pokemon }) => <PokeCard key={pokemon.id} pokemon={pokemon} species={species} />)}
             </div>
             <PokePagination currentPage={pageNumber} totalPages={totalPages} />
         </div>
